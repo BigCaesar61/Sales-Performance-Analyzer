f1806ed622ffc901edeb2893370514b1be4881cd
Martin Leone <martinnikolaslll@gmail.com>
2024-09-07 09:38:27 -0400

    Create determinePerformanceRating function

--- Sales_Performance_Analyzer.js
+++ Sales_Performance_Analyzer.js
@@ -6,3 +6,27 @@ function calculateAverageSales(sales)
     const totalSales = sales.reduce((acc,sale) => acc + sale, 0);
     return totalSales/ sales.length
 }
+
+//Task 2: Create a Function to Determine Performance Rating
+
+function determinePerformanceRating(averageSales) 
+{   
+    if (averageSales > 10000)
+    {
+        return "Excellent";
+    } 
+    else if (averageSales >= 7000) 
+    {
+        return "Good";
+    }
+    else if (averageSales >= 4000)
+    {   
+        return "Satisfactory";
+
+    }
+    else 
+    {   
+        return "needs Improvement";
+    }
+
+}
